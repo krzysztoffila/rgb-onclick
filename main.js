707dc9c7cf174cdeb2ea174f41249c2406b037db
krzysztoffila <umowy.fila@gmail.com>
2023-02-27 19:11:02 +0100

    v1

--- main.js
+++ main.js
@@ -3,3 +3,17 @@
 // jeśli wartość dla x (event.clientX) i y (event.clientY) jest parzysta (np. 100,122)  to kolor czerwony
 // jesli wartość dla x i y jest nieparzysta to kolor niebieski
 // jeśli jeden z kolorów jest parzysty a drugi nieparzysty to kolor zielony
+
+window.addEventListener('click', event => {
+    const x = event.clientX + 1;
+    const y = event.clientY + 1;
+    console.log(x, y)
+    if (x % 2 == 0 && y % 2 == 0) {
+        document.body.style.backgroundColor = 'red'
+    }
+    if (x % 2 !== 0 && y % 2 !== 0) {
+        document.body.style.backgroundColor = 'blue'
+    } else {
+        document.body.style.backgroundColor = 'green'
+    }
+})
